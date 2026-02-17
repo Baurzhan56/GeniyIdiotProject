576b6a8ddcf0caa9f0b267c0e0d2da1d134fd20c
Baurzhan56 <killerbaur@mail.ru>
2026-02-17 10:43:21 +0500

    Сделал подзадачу

--- GeniiIdiotConsoleApp/Program.cs
+++ GeniiIdiotConsoleApp/Program.cs
@@ -1,13 +1,35 @@
-﻿using System.Data;
-using System.Security.Cryptography;
-
-namespace GeniiIdiotConsoleApp
+﻿namespace GeniiIdiotConsoleApp
 {
-    class Program
+    class Program// надо пока оставить название ка есть Program. Что выходит из текущего навзания, что класс содержит смешанные вопросы, а по факту...? А по факту,
+                     // здесь вся логика игры. Такое название неверное.
     {
-        static string[] GetQuestions(int countQuestion)
+        static void Main(string[] args)
         {
-            string[] questions = new string[countQuestion];
+            Console.WriteLine("Введите ваше имя пожалуйста.");
+            var name = Console.ReadLine();
+            do
+            {
+                ShuffleQuestion(out string[] questions, out int[] answers);
+                var RightAnswersCount = 0;// в навзании перменной главное слово  в конце, здесь главное слово количество
+                for (int i = 0; i < questions.Length; i++)
+                {
+                    Console.WriteLine("Номер вопроса: " + (i + 1));
+                    Console.WriteLine(questions[i]);
+                    var userAnswer = GetAnswer();
+                    var rightAnswer = answers[i];
+                    if (userAnswer == rightAnswer)
+                    {
+                        RightAnswersCount++;
+                    }
+                }
+                Console.WriteLine($"Количество правильных ответов: {RightAnswersCount}{Environment.NewLine}Ваш диагноз {name}: " + GetDiagnose(RightAnswersCount) + $"{Environment.NewLine}" + $"{name} не хотите ли вы сыграть снова? Ответьте да или нет?");// /n в некоторых системах
+                // работает некорректно. Как сделать более правильные перенос на новую строку.
+            }
+            while (IsContinue());
+        }
+        public static string[] GetQuestions()// все методы с модификаторами!!! Как думаете, если мы ответы привяжем к вопросам, нам уже не надо будет все пермешивать и ответы и вопросы? Это доп задание!
+        {
+            var questions = new string[5];
             questions[0] = "Сколько будет два плюс два умноженное на два?";
             questions[1] = "Бревно нужно распилить на 10 частей , сколько надо сделать надпилов?";
             questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
@@ -15,9 +37,9 @@ namespace GeniiIdiotConsoleApp
             questions[4] = "Пять свечей горело, две потухли. Сколько свечей осталось?";
             return questions;
         }
-        static int[] GetAnswers(int countQuestion)
-        {   
-            int[] answers = new int[countQuestion];
+        public static int[] GetAnswers()// все методы с модификаторами!!! Попробуем объединить с вопросами
+        {
+            var answers = new int[5];
             answers[0] = 6;
             answers[1] = 9;
             answers[2] = 25;
@@ -25,68 +47,77 @@ namespace GeniiIdiotConsoleApp
             answers[4] = 2;
             return answers;
         }
-        static string GetDiagnose(int countRightAnswers)
+        public static string GetDiagnose(int countRightAnswers)// все методы с модификаторами!!!
         {
-
-            string[] diagnose = new string[6];
+            var questions = GetQuestions();
+            var percentage = (double)countRightAnswers * 100 / questions.Length;// var
+            var diagnose = new string[6];
             diagnose[0] = "Идиот";
             diagnose[1] = "Кретин";
             diagnose[2] = "Дурак";
             diagnose[3] = "Нормально";
             diagnose[4] = "Талант";
             diagnose[5] = "Гений";
-            return diagnose[countRightAnswers];
-        }
-        public class RandomClass
-        {
-            public static Random rand = new Random();
-            public static List<int> List = new List<int>();
-            public static int RandomNumb(int count)
+            switch (percentage)// не надо смешивать в одном методе диагнозы и рассчет диагноза. Это разные вещи, надо разделить и подумать как еще можной упростить расчет диагноза. ИИ лучше не использовать, она оставляет метки))))
             {
-                int random = 0;
-                do
-                {
-                    random = rand.Next(0, count);
-                }
-                while (List.Contains(random));
-                List.Add(random);
-                return random;
+                case < 20: return diagnose[0];
+                case >= 20 and < 40: return diagnose[1];
+                case >= 40 and < 60: return diagnose[2];
+                case >= 60 and < 80: return diagnose[3];
+                case >= 80 and < 100: return diagnose[4];
+                case >= 100: return diagnose[5];
+                default:
+                    return "Mistake";
             }
-            public static void Clear()
-            {
-                List.Clear();
-            }
+            ;
         }
-        static void Main(string[] args)
+        public static void ShuffleQuestion(out string[] questions, out int[] answers)// все методы с модификаторами!!! Название метода не очень ShuffleQuestions
         {
-            Console.WriteLine("Введите ваше имя пожалуйста.");
-            string name = Console.ReadLine();
-            int countQuestion = 5;
-            string[] questions = GetQuestions(countQuestion);
-            int[] answers = GetAnswers(countQuestion);
-            RandomClass randomClass = new RandomClass();
-            string Repeat = string.Empty;
-            do
-            {
-                int countRightAnswers = 0;
-                RandomClass.Clear();
-                for (int i = 0; i < countQuestion; i++)
-                {
-                    int random = RandomClass.RandomNumb(countQuestion);
-                    Console.WriteLine("Номер вопроса: " + (i + 1));
-                    Console.WriteLine(questions[random]);
-                    int userAnswer = Convert.ToInt32(Console.ReadLine());
-                    int rightAnswer = answers[random];
-                    if (userAnswer == rightAnswer)
-                    {
-                        countRightAnswers++;
-                    }
+            Random rand = new Random();
+            questions = GetQuestions();
+            answers = GetAnswers();
+            string temporaryString;
+            int temporaryInt = 0;
 
-                }
-                Console.WriteLine($"Количество правильных ответов: {countRightAnswers}\nВаш диагноз {name}: " + GetDiagnose(countRightAnswers));
-                Console.WriteLine($"{name} не хотите ли вы сыграть снова? Ответьте да или нет.");
-                Repeat = Console.ReadLine();
-            } while (Repeat.ToLower() == "да");
+            for (int i = questions.Length - 1; i > 0; i--)
+            {
+                int j = rand.Next(0, i);
+                temporaryString = questions[j];
+                questions[j] = questions[i];
+                questions[i] = temporaryString;
+
+                temporaryInt = answers[j];
+                answers[j] = answers[i];
+                answers[i] = temporaryInt;
+            }
         }
+        public static bool IsContinue()// все методы с модификаторами!!! Навзание метода не очень IsContinue. 
+        {
+            while (true)
+            {
+                string answer = Console.ReadLine().ToLower().Trim();
+                switch (answer)
+                {
+                    case "да": return true;
+                    case "нет": return false;
+                    default:
+                        Console.WriteLine("Некорректный ответ.Попробуйте ещё раз ввести ответ. Да или Нет?");
+                        break;
+                }
+            }
+        }
+        public static int GetAnswer() // все методы с модификаторами!!! Мы же здесь GetAnswer делаем, просто пытаемся получить ответ
+        {
+            while (true)
+            {
+                string answer = Console.ReadLine();
+                if (int.TryParse(answer, out int result))
+                {
+                    return result;
+                }
+                Console.WriteLine("Некорректный ввод. Введите число.");
+            }
+        }
+
     }
 }

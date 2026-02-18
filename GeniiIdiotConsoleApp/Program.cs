ee47ef81e9f1b3240c294780e4e2948506b9c843
Baurzhan56 <killerbaur@mail.ru>
2026-02-18 15:54:00 +0500

    Correctivness from Mentor

--- GeniiIdiotConsoleApp/Program.cs
+++ GeniiIdiotConsoleApp/Program.cs
@@ -1,7 +1,6 @@
 ﻿namespace GeniiIdiotConsoleApp
 {
-    class Program// надо пока оставить название ка есть Program. Что выходит из текущего навзания, что класс содержит смешанные вопросы, а по факту...? А по факту,
-                     // здесь вся логика игры. Такое название неверное.
+    class Program
     {
         static void Main(string[] args)
         {
@@ -10,7 +9,7 @@
             do
             {
                 var questions = ShuffleQuestion();
-                var RightAnswersCount = 0;// в навзании перменной главное слово  в конце, здесь главное слово количество
+                var rightAnswersCount = 0;// переменные с маленькой буквы
                 for (int i = 0; i < questions.Count; i++)
                 {
                     Console.WriteLine("Номер вопроса: " + (i + 1));
@@ -21,30 +20,30 @@
                         var rightAnswer = dict.Value;
                         if (userAnswer == rightAnswer)
                         {
-                            RightAnswersCount++;
+                            rightAnswersCount++;
                         }
                     }
                 }
-                Console.WriteLine($"Количество правильных ответов: {RightAnswersCount}{Environment.NewLine}Ваш диагноз {name}: " + GetDiagnose(RightAnswersCount) + $"{Environment.NewLine}" + $"{name} не хотите ли вы сыграть снова? Ответьте да или нет?");// /n в некоторых системах
-                // работает некорректно. Как сделать более правильные перенос на новую строку.
+                Console.WriteLine($"Количество правильных ответов: {rightAnswersCount}{Environment.NewLine}Ваш диагноз {name}: " + GetDiagnose(rightAnswersCount) + $"{Environment.NewLine}" + $"{name} не хотите ли вы сыграть снова? Ответьте да или нет?");// /n в некоторых системах
+
             }
-            while (IsContinue());//Для тестирования гита
+            while (IsContinue());
         }
-        public static List<Dictionary<string,int>> GetQuestions()// все методы с модификаторами!!! Как думаете, если мы ответы привяжем к вопросам, нам уже не надо будет все пермешивать и ответы и вопросы? Это доп задание!
+        public static List<Dictionary<string, int>> GetQA()//  теперь это метод с вопросами и ответами)))) может быть переименуем?)
         {
             var answer1 = new Dictionary<string, int> { { "Сколько будет два плюс два умноженное на два?", 6 } };
-            var answer2 = new Dictionary<string, int> { {"Бревно нужно распилить на 10 частей , сколько надо сделать надпилов?", 9 } };
+            var answer2 = new Dictionary<string, int> { { "Бревно нужно распилить на 10 частей , сколько надо сделать надпилов?", 9 } };
             var answer3 = new Dictionary<string, int> { { "На двух руках 10 пальцев. Сколько пальцев на 5 руках?", 25 } };
-            var answer4 = new Dictionary<string, int> { {"Укол делают каждые полчаса, сколько нужно минут для трёх уколов?", 60 } };
+            var answer4 = new Dictionary<string, int> { { "Укол делают каждые полчаса, сколько нужно минут для трёх уколов?", 60 } };
             var answer5 = new Dictionary<string, int> { { "Пять свечей горело, две потухли. Сколько свечей осталось?", 2 } };
             var answers = new List<Dictionary<string, int>>{
             answer1,answer2,answer3,answer4,answer5};
             return answers;
         }
-        public static string GetDiagnose(int countRightAnswers)// все методы с модификаторами!!!
+        public static string GetDiagnose(int countRightAnswers)
         {
-            var questions = GetQuestions();
-            var percentage = (double)countRightAnswers * 100 / questions.Count;// var
+            var questions = GetQA();// зачем здесь снеова вытаскивать вопросы, это тарта памяти. В метода надо сразу передавать данные о количестве вопросов для расчета диагноза
+            var percentage = (double)countRightAnswers * 100 / questions.Count;
             var diagnose = new string[6];
             diagnose[0] = "Идиот";
             diagnose[1] = "Кретин";
@@ -52,6 +51,8 @@
             diagnose[3] = "Нормально";
             diagnose[4] = "Талант";
             diagnose[5] = "Гений";
+
+            // вот этот блок кода в отдельный метода расчета CalculateDiagnose, как пример
             switch (percentage)// не надо смешивать в одном методе диагнозы и рассчет диагноза. Это разные вещи, надо разделить и подумать как еще можной упростить расчет диагноза. ИИ лучше не использовать, она оставляет метки))))
             {
                 case < 20: return diagnose[0];
@@ -61,24 +62,25 @@
                 case >= 80 and < 100: return diagnose[4];
                 case >= 100: return diagnose[5];
                 default:
-                    return "Mistake";
+                    return "Mistake";// это слов здесь не очень. Как минимум оно должно быть на русском и чтот обозначать при дефолтном значении
             }
             ;
+            //
         }
-        public static List<Dictionary<string,int>> ShuffleQuestion()// все методы с модификаторами!!! Название метода не очень ShuffleQuestions
+        public static List<Dictionary<string, int>> ShuffleQuestion()
         {
             Random rand = new Random();
-            var result = GetQuestions();
-            for (int i = result.Count-1; i >= 0; i--)
+            var result = GetQA();
+            for (int i = result.Count - 1; i >= 0; i--)
             {
-                int j = rand.Next(0, i);
+                int j = rand.Next(0, i);// var
                 var temp = result[j];
                 result[j] = result[i];
                 result[i] = temp;
             }
             return result;
         }
-        public static bool IsContinue()// все методы с модификаторами!!! Навзание метода не очень IsContinue. 
+        public static bool IsContinue()
         {
             while (true)
             {
@@ -93,7 +95,7 @@
                 }
             }
         }
-        public static int GetAnswer() // все методы с модификаторами!!! Мы же здесь GetAnswer делаем, просто пытаемся получить ответ
+        public static int GetAnswer()
         {
             while (true)
             {
